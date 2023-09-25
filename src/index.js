9d9c31dd85209867d6c0a2a6a49c7e0d25c3bf16
Visotsky Maxim <needlife124@gmail.com>
2023-09-25 14:45:25 +0300

    upgrate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import swiper from './swiper';
+
 const menuBtnRef = document.querySelector('[data-menu-button]');
 const mobileMenuRef = document.querySelector('[data-menu]');
+console.log(mobileMenuRef);
 
 menuBtnRef.addEventListener('click', () => {
   const expanded = menuBtnRef.getAttribute('aria-expanded') === true || false;
@@ -8,27 +11,4 @@ menuBtnRef.addEventListener('click', () => {
   menuBtnRef.setAttribute('aria-expanded', !expanded);
 
   mobileMenuRef.classList.toggle('is-open');
-});
-
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
---
+++ src/swiper.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  speed: 1500,
+  loop: true,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+});
