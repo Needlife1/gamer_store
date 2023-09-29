9e592a4ef8db8e45eccc1b8db675a55afd48c887
Needlife1 <111612083+Needlife1@users.noreply.github.com>
2023-09-29 10:39:14 +0000

    Deploying to gh-pages from @ Needlife1/gamer_store@2b5e855a8d2cb5e5056fcb3de92877c9218209f5 🚀

--- index.a1280944.js
+++
@@ -1,2 +0,0 @@
-!function(){new Swiper(".swiper",{pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},speed:1500,loop:!0,effect:"fade",fadeEffect:{crossFade:!0}});var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");console.log(t),e.addEventListener("click",(function(){var n=!0===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open")}))}();
-//# sourceMappingURL=index.a1280944.js.map
